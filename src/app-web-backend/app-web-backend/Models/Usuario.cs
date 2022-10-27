42be4db469e185c581a673f6cbcdbc30eec76122
Renato Silva <renatoserra7@gmail.com>
2022-10-27 19:52:10 -0300

    Alterando backend

--- src/app-web-backend/app-web-backend/Models/Usuario.cs
+++ src/app-web-backend/app-web-backend/Models/Usuario.cs
@@ -13,13 +13,14 @@ namespace app_web_backend.Models
         [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Obrigatório Informar o nome!")]
+        [Required(ErrorMessage = "Informe o nome do usuário!", AllowEmptyStrings = false)]
         public string Nome { get; set; }
 
+        [RegularExpression(@"^([0-9a-zA-Z]([\+\-_\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]*\.)+[a-zA-Z0-9]{2,3})$", ErrorMessage = "Informe um email válido.")]
         [Required(ErrorMessage = "Obrigatório Informar o email!")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Obrigatório Informar a senha!")]
+        [Required(ErrorMessage = "Informe a senha do usuário", AllowEmptyStrings = false)]
         [DataType(DataType.Password)]
         public string Senha { get; set; }
 
